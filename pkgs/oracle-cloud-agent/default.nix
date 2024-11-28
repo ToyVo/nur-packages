59e1cad59baef49dc3625b7dca368f9652359fec
Collin Diekvoss <Collin@Diekvoss.com>
2024-11-28 10:03:46 -0600

    pkgs

--- pkgs/oracle-cloud-agent/default.nix
+++ pkgs/oracle-cloud-agent/default.nix
@@ -1,9 +1,5 @@
-{ stdenv }:
+{ pkgs }:
 
-stdenv.mkDerivation rec {
-  name = "oracle-cloud-agent-${version}";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
-}
+pkgs.writeShellScriptBin "oracle-cloud-agent" ''
+  echo Hello World
+'';
